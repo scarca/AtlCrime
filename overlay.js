9bb72653b95cd45c5fcea37ee7207146c4fade74
Raj Shrimali <rajshrimali@gmail.com>
2018-02-16 15:37:51 -0500

    updated parameters

--- overlay.js
+++ overlay.js
@@ -63,7 +63,7 @@ ContourOverlay.prototype.onAdd = function() {
         nd.push(cliff);
         nd.unshift(cliff);
       });
-  this.zs = [36, 73, 123, 192, 313, 495, 820, 1186, 1358.0];
+  this.zs = [11, 36, 73, 123, 192, 313, 495, 820, 1358.0];
   this.colours = ['#000099','#0000FF','#3399FF','#00CCFF','#00CC00','#66FF00','#FFFF00','#CC0000','#FF6633'],
   this.c.contour(d, 0, xs.length - 1, 0, ys.length - 1, xs, ys, this.zs.length, this.zs);
 }
